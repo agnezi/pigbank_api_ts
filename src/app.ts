5d9b2d08c8e6666ecc87ddbf8efd167d7da804ea
agnezi <tkoff@localhost.localdomain>
2020-03-15 13:40:48 -0300

    enviroment config

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,5 @@ import 'reflect-metadata';
 import app from './server';
 dotenv.config();
 
-app.listen(process.env.DEV_PORT);
+app.listen(process.env['NODE_ENV'] === 'development' ? process.env.DEV_PORT : process.env.PROD_PORT);
 console.log(`Server liste on port: ${process.env.DEV_PORT}`)
